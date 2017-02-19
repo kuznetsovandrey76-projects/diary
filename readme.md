0806f6d7b2a1394d9cc5d079854ef7189d33463e
kuznetsovandrey76 <and.rey.q@yandex.ru>
2017-02-19 22:09:13 +0300

    init diary

---
+++ readme.md
@@ -0,0 +1,31 @@
+1. создаем repo на heroku
+
+2. npm init, создаем package.json
+
+3. устанавливаем модули 
+npm install --save body-parser express express-handlebars mongodb mongoose
+
+4. подключаем все зависимости в server.js
+
+5. создаем БД, добавляем User и подключаем
+mongoose.connect('mongodb://<dbuser>:<dbpassword>...');
+
+6. устанавливаем порт, корневыю директорию и парсер
+app.set('port', process.env.PORT || 8080);
+все href и src отталкиваются от папки /public
+app.use(express.static(__dirname + '/public'));	
+app.use(bodyParser.json());
+
+7. в самый конец добавляем 404, 500, app.listen()
+
+8. добавляем post и get
+
+9. добавляем модель поста (на сервере к YOUR-COLLECTOIONS-NAME добавляется s если ее там нет)
+var Post = mongoose.model('YOUR-COLLECTOIONS-NAME', { 
+	title: String,
+	text: String 
+});
+
+10. mkdir views/layouts public/{css,img} my_modules -p
+
+11.	touch views/layouts/main.handlebars views/{404.handlebars,500.handlebars,index.handlebars,blog.handlebars} public/css/style.css my_modules/db.js .gitignore
---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+var db = require('./my_modules/db.js');
+const bodyParser = require('body-parser');
+
+var handlebars  = require('express-handlebars')
+	.create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var mongoose = require('mongoose');
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>...');
+mongoose.connect(db.url);
+
+var Post = mongoose.model('diary-programming', { 
+	day: String,
+	progress: String,
+	thoughts: String,
+	link: String
+});
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(__dirname + '/public'));	
+app.use(bodyParser.json());
+
+// POST
+app.post('/save', function(req, res) {
+	var newPost = new Post(req.body);
+
+	newPost.save(function (err) {
+		// необязательная функция	
+	  if (err) { console.log(err); } 
+	  else { console.log('данные загружены в базу'); }
+	});
+	res.end();
+});
+
+// GET
+app.get(['/', '/index.html'], function(req, res) {
+	res.render('index');
+});
+
+app.get('/blog', function(req, res) {
+	Post.find({}).then(function(local) {
+		res.render('blog', {items: local});
+	});
+});
+
+// NOT FOUND
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	res.status(500);
+	res.render('500');
+});
+
+// NODE SERVER.JS
+app.listen(app.get('port'), function() {
+	console.log('Express запущен на http://localhost:' + 
+		app.get('port') + ': нажмите CTRL+C для завершения');
+});
